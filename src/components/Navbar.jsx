4e6a31d560af56597735fc157f1a8566ea639a3c
PasinduPabasaraSL <pabasarapasindu365@gmail.com>
2026-06-07 10:47:18 +0530

    Add Logo icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,22 @@
 import Button from "./Button";
+import Logo from "../assets/logo.jpeg"
 
 const links = ["home", "about", "structure", "timeline", "prizes", "team"];
 
 export default function Navbar() {
     return (
-        <nav className="navbar">
+        <nav className="navbar flex items-center gap-3">
             {/* Logo */}
             <div className="navbar-logo">
-                <span className="logo-icon">◼</span>
+                <img
+                    src={Logo}
+                    alt="OctWave Logo"
+                    className="w-10 h-10 object-contain"
+                />
                 <span className="logo-text">
-          OCT<span className="logo-accent">WAVE</span>{" "}
+                    OCT<span className="logo-accent">WAVE</span>{" "}
                     <span className="logo-version">3.0</span>
-        </span>
+                </span>
             </div>
 
             {/* Links */}
